4c226ecc8a5e5a62b2b82f854fe136e3207c0b6c
AzureAD\윤주영 <pulpul8282@naver.com>
2021-12-29 17:15:19 +0900

    MQ 메시지 생성 및 소비 테스트 완료

---
+++ src/main/java/com/example/service/Consumer.java
@@ -0,0 +1,15 @@
+/*
+ * package com.example.service;
+ * 
+ * import org.slf4j.Logger; import org.slf4j.LoggerFactory; import
+ * org.springframework.jms.annotation.JmsListener; import
+ * org.springframework.stereotype.Component;
+ * 
+ * @Component public class Consumer {
+ * 
+ * private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
+ * 
+ * @JmsListener(destination = "Yoon-ju-young-queue") public void
+ * consumeMessage(String message) {
+ * logger.info("Message received from activemq queue---"+message); } }
+ */
---
+++ src/main/java/com/example/service/NewConsumer.java
@@ -0,0 +1,40 @@
+package com.example.service;
+
+import javax.jms.Queue;
+
+import javax.jms.Queue;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dto.Student;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+@RequestMapping("/consume")
+public class NewConsumer {
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
+
+	@Autowired
+	private Queue queue;
+
+	@GetMapping("/message")
+	public Student consumeMessage() {
+
+		Student student = null;
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			String jsonMessage = (String) jmsTemplate.receiveAndConvert(queue);
+			student = mapper.readValue(jsonMessage, Student.class);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return student;
+	}
+}
